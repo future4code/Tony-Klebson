952c7cfd7623fb9270bfe40b7ff5ef32e59d12e0
tonyklebson <tony.klebson@hotmail.com>
2021-10-21 23:28:46 -0300

    Exercicios Flex e Grid

---
+++ quinzena4/fixacao.js
@@ -0,0 +1,17 @@
+function contaOcorrencias(arrayDeNumeros, numeroEscolhido) {
+    // Escreva seu código aqui
+    let numeroRepetido =0
+    //for (let numero of arrayDeNumeros)
+    if (arrayDeNumeros===numeroEscolhido){
+    numeroRepetido++
+    }
+  
+  
+  if (numeroRepetido >0){
+    return `O numero ${numeroEscolhido} aparece ${numeroRepetido}x`
+  } 
+  
+  else (numero===3){
+  return `Numero não encontrado`
+  }
+  }
