4a25361a136f9c0dfa6ef844638c9ab9c679c52e
tonyklebson <tony.klebson@hotmail.com>
2021-11-24 00:16:01 -0300

    rend-condicional-form

---
+++ rend-condicional-form/nome-do-seu-app/src/components/Etapa4.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Etapa4() {
+  return (
+    <div className="App">
+      <h1>O FORMULÁRIO ACABOU!</h1>
+      <P>Muito obrigado por participar! Entraremos em contato</P>
+    </div>
+  );
+}
+
+export default Etapa4;
